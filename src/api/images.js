eb58145b92f59ab811d11a748fe21c57e07eefe9
leonardo <leonardotobe94[1;7r[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D@gmail.com>
2021-08-23 19:11:32 -0400

    fix position images

--- src/api/images.js
+++ src/api/images.js
@@ -2,7 +2,7 @@
 export const hairCuts = [
   {
     className: 'carousel__item',
-    dataPos: '-2',
+    dataPos: '0',
     src: 'https://res.cloudinary.com/djc1umong/image/upload/v1629756630/corte_bdqme7.webp',
     alt: 'Corte de pelo por Nelson peluqueria Spa | Bolivia-Cochabamba'
   },
@@ -17,13 +17,13 @@ export const hairCuts = [
 export const balayage = [
   {
     className: 'carousel__item',
-    dataPos: '-2',
+    dataPos: '0',
     src: 'https://res.cloudinary.com/djc1umong/image/upload/v1629757752/mechasBalayagesPSD_wziza8.webp',
     alt: 'Hermoso Balayage por Nelson peluqueria Spa | Bolivia-Cochabamba'
   },
   {
     className: 'carousel__item',
-    dataPos: '-1',
+    dataPos: '1',
     src: 'https://res.cloudinary.com/djc1umong/image/upload/v1629757831/mechasBalayage_1_uq0f06.webp',
     alt: 'Hermoso Balayage tipo Mechas de pelo por Nelson peluqueria Spa | Bolivia-Cochabamba'
   },
@@ -38,13 +38,13 @@ export const balayage = [
 export const mechas = [
   {
     className: 'carousel__item',
-    dataPos: '-2',
+    dataPos: '1',
     src: 'https://res.cloudinary.com/djc1umong/image/upload/v1629758969/mechasYBarrido_ukbo65.webp',
     alt: 'Hermosas Mechas barridas en un degradado elegante por Nelson peluqueria Spa | Bolivia-Cochabamba'
   },
   {
     className: 'carousel__item',
-    dataPos: '-1',
+    dataPos: '0',
     src: 'https://res.cloudinary.com/djc1umong/image/upload/v1629758943/mechas_k41rij.webp',
     alt: 'Hermosas Mechas barridas en un degradado elegante por Nelson peluqueria Spa | Bolivia-Cochabamba'
   },
