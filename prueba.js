9284846509c58ba3d54307c0f9c733e3fddac1d3
leonardo <leonardotobe94[1;7r[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D@gmail.com>
2021-08-24 11:40:31 -0400

    Add new meta tags

---
+++ prueba.js
@@ -0,0 +1,17 @@
+const list = ['💻', '🦕', '🔭', '🧀']
+
+// list.forEach(emoji => {
+//   console.log(emoji + '🔥')
+// })
+
+// const newList = list.map(emoji => emoji + '🔥')
+
+// const newList = list.filter(emoji => emoji !== '🦕')
+
+const newList = list.fill('🔥')
+console.log(newList)
+
+// forEach
+// map()
+// filter()
+// fill()
